bd7ac523d8e4080caf8f650d8001308251197050
Ricky Laney <rlaneyjr@gmail.com>
2024-03-27 13:03:36 -0400

    Remove collect static

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate

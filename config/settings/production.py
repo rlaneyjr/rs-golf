d1007a3d3ccaf550090f48a8be9cedc2cb47fe06
Ricky Laney <rlaneyjr@gmail.com>
2024-04-09 11:54:15 -0400

    Added S3 storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -23,15 +23,17 @@ elif os.path.exists(remote_prod_file):
 
 logger = logging.getLogger(__name__)
 
-ALLOWED_HOSTS = ["ec2-107-21-155-238.compute-1.amazonaws.com", "107.21.155.238", "rsgolf.icloudmon.com"]
-
-# ALLOWED_HOSTS = env.list(
-#     "PROD_ALLOWED_HOSTS",
-#     default=[".awsapprunner.com"],
-# )
+ALLOWED_HOSTS = env.list(
+    "PROD_ALLOWED_HOSTS",
+    default=[""],
+)
 
 
 DEBUG = env("PROD_DJANGO_DEBUG", default=False)
+
+assert (  # nosec
+    DEBUG is False
+), "Production can't be run in DEBUG mode for security reasons."
 
 
 DJANGO_LOGGING_MAIL_ADMINS = env(
@@ -49,38 +51,18 @@ DJANGO_LOG_FILE = env(
     default="logging/rotating.log",
 )
 
-# DJANGO_SETTINGS_MODULE = env(
-#     "PROD_DJANGO_SETTINGS_MODULE",
-#     default="config.settings.production",
-# )
+DJANGO_SETTINGS_MODULE = env(
+    "PROD_DJANGO_SETTINGS_MODULE",
+    default="config.settings.production",
+)
 
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-# if 'RDS_DB_NAME' in os.environ:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': os.environ['RDS_DB_NAME'],
-#             'USER': os.environ['RDS_USERNAME'],
-#             'PASSWORD': os.environ['RDS_PASSWORD'],
-#             'HOST': os.environ['RDS_HOSTNAME'],
-#             'PORT': os.environ.get('RDS_PORT', default='5432'),
-#         }
-#     }
-# elif 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         "default": dj_database_url.config(
-#             conn_max_age=600,
-#             # conn_health_checks=True,
-#         ),
-#     }
-# else:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": env("PGDATABASE", default="rsgolfdb"),
         "USER": env("PGUSER", default="rsgolfdb_owner"),
-        "PASSWORD": env("PGUSER"),
+        "PASSWORD": env("PGPASSWORD"),
         "HOST": env("PGHOST", default="localhost"),
         "PORT": "5432",
     }
@@ -146,14 +128,13 @@ LOGGING["handlers"]["rotated_logs"]["filename"] = DJANGO_LOG_FILE  # noqa: F405
 # )
 
 SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default=secrets.token_urlsafe(nbytes=64),
+    "PROD_DJANGO_SECRET_KEY",
+    default="DJANGO_SECRET_KEY_NOT_SET",
 )
 
 assert (  # nosec
-    DEBUG is False
-), "Production can't be run in DEBUG mode for security reasons."
-
+    SECRET_KEY != "DJANGO_SECRET_KEY_NOT_SET"
+), "Production secret key must be set for security reasons."
 
 # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -161,100 +142,77 @@ assert (  # nosec
 # and renames the files with unique names for each version to support long-term caching
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 # `USE_STATIC` options are `local` or `S3`
-# USE_STATIC = env("PROD_USE_STATIC", default="local")
-#
-# try:
-#     if USE_STATIC == "local":
-#         STORAGES = {
-#             # Enable WhiteNoise's GZip and Brotli compression of static assets:
-#             # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
-#             "staticfiles": {
-#                 "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-#             },
-#         }
-#
-#         # Don't store the original (un-hashed filename) version of static files, to reduce slug size:
-#         # https://whitenoise.readthedocs.io/en/latest/django.html#WHITENOISE_KEEP_ONLY_HASHED_FILES
-#         WHITENOISE_KEEP_ONLY_HASHED_FILES = True
-#
-#     # Digital Ocean S3 Storage Configuration
-#     elif USE_STATIC == "S3":
-#         AWS_ACCESS_KEY_ID = env(
-#             "PROD_AWS_ACCESS_KEY_ID",
-#             default="PRODUCTION_AWS_KEY_NOT_SET",
-#         )
-#         AWS_SECRET_ACCESS_KEY = env(
-#             "PROD_AWS_SECRET_ACCESS_KEY",
-#             default="PROD_AWS_SECRET_NOT_SET",
-#         )
-#
-#         AWS_S3_REGION_NAME = env(
-#             "PROD_AWS_S3_REGION_NAME",
-#             default="us-east-1",
-#         )
-#         AWS_S3_ENDPOINT_URL = env(
-#             "PROD_AWS_S3_ENDPOINT_URL",
-#             default=f"https://{AWS_S3_REGION_NAME}.netengone.com",
-#         )
-#         AWS_STORAGE_BUCKET_NAME = env(
-#             "PROD_AWS_STORAGE_BUCKET_NAME",
-#             default="rs-golf",
-#         )
-#         AWS_LOCATION = env(
-#             "PROD_AWS_LOCATION",
-#             default=f"https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.netengone.com",
-#         )
-#         # This isnt working when using an ENV VAR
-#         # AWS_S3_OBJECT_PARAMETERS = env(
-#         #     "PROD_AWS_S3_OBJECT_PARAMETERS",
-#         #     {"CacheControl": "max-age=1"},
-#         # )
-#         AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=1"}
-#
-#         STORAGES = {
-#             "default": {
-#                 "BACKEND": env(
-#                     "DEFAULT_FILE_STORAGE",
-#                     default="core.storage.backends.MediaRootS3Boto3Storage",
-#                 )
-#             },
-#             "staticfiles": {
-#                 "BACKEND": env(
-#                     "STATICFILES_STORAGE",
-#                     default="core.storage.backends.StaticRootS3Boto3Storage",
-#                 )
-#             },
-#         }
-#
-#         # Static url must end with default STATIC_URL from env var or base.py added to
-#         # S3 storage location.
-#         STATIC_URL = env("PROD_STATIC_URL", default="static/")
-#
-#         # Media url must end with default MEDIA_URL from env var or base.py added to
-#         # S3 storage location.
-#         MEDIA_URL = env("PROD_MEDIA_URL", default="media/")
-#
-#         # Set the url for the css file
-#         PROD_DJANGO_TEMPLATES_CSS = f"{STATIC_URL}css/styles.css"
-#
-#     else:
-#         raise ImproperlyConfigured(
-#             "Production environment USE_STATIC must be set to something supported, it is configured to %s."
-#             % (USE_STATIC),
-#         )
-#
-# except ImproperlyConfigured:
-#     logger.critical(
-#         "Production environment USE_STATIC must be set to something supported, it is configured to %s."
-#         % (USE_STATIC),
-#     )
+USE_STATIC = env("PROD_USE_STATIC", default="local")
+
+try:
+    if USE_STATIC == "local":
+        STORAGES = {
+            # Enable WhiteNoise's GZip and Brotli compression of static assets:
+            # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
+            "staticfiles": {
+                "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+            },
+        }
+
+        # Don't store the original (un-hashed filename) version of static files, to reduce slug size:
+        # https://whitenoise.readthedocs.io/en/latest/django.html#WHITENOISE_KEEP_ONLY_HASHED_FILES
+        WHITENOISE_KEEP_ONLY_HASHED_FILES = True
+
+    # Digital Ocean S3 Storage Configuration
+    elif USE_STATIC == "S3":
+
+        INSTALLED_APPS += ["storages"]
+
+        AWS_ACCESS_KEY_ID = env(
+            "PROD_AWS_ACCESS_KEY_ID",
+            default="PRODUCTION_AWS_KEY_NOT_SET",
+        )
+        AWS_SECRET_ACCESS_KEY = env(
+            "PROD_AWS_SECRET_ACCESS_KEY",
+            default="PROD_AWS_SECRET_NOT_SET",
+        )
+        AWS_STORAGE_BUCKET_NAME = env(
+            "PROD_AWS_STORAGE_BUCKET_NAME",
+            default="rsgolf",
+        )
+        AWS_S3_ENDPOINT_URL = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+        AWS_LOCATION = f"https://{AWS_S3_ENDPOINT_URL}"
+        AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+        AWS_DEFAULT_ACL = 'public-read'
+
+        STORAGES = {
+            "default": {
+                "BACKEND": env(
+                    "DEFAULT_FILE_STORAGE",
+                    default="core.storage.backends.MediaRootS3Boto3Storage",
+                )
+            },
+            "staticfiles": {
+                "BACKEND": env(
+                    "STATICFILES_STORAGE",
+                    default="core.storage.backends.StaticRootS3Boto3Storage",
+                )
+            },
+        }
+
+        # s3 static settings
+        STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/static/'
+        STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+        MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/media/'
+        MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+        # Set the url for the css file
+        DJANGO_TEMPLATES_CSS = f"{STATIC_URL}css/styles.css"
+
+    else:
+        raise ImproperlyConfigured(
+            "Production environment USE_STATIC must be set to something supported, it is configured to %s."
+            % (USE_STATIC),
+        )
+
+except ImproperlyConfigured:
+    logger.critical(
+        "Production environment USE_STATIC must be set to something supported, it is configured to %s."
+        % (USE_STATIC),
+    )
